f5355ea1b467b7676337d453854e1e3ed3f76a5f
Xiena Happy <longandhappylife@mail.ru>
2022-04-04 17:37:59 +0300

    WebUIJavaGBKitaevaHW1

---
+++ src/main/java/org/github/XienaHappy/App.java
@@ -0,0 +1,13 @@
+package org.github.XienaHappy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
